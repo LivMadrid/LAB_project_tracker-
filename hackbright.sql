08f7bf8baee978e82cbc349b3cbeb5f5e17bf06e
Olivia Boynton <liv.madrid14@gmail.com>
2021-01-27 20:13:46 +0000

    Initial Commit/ Create project database      Co-Authored by Rachel Elysia Perkins <rachel.elysia.perkins@gmail.com>

---
+++ hackbright.sql
@@ -0,0 +1,179 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.15 (Ubuntu 10.15-0ubuntu0.18.04.1)
+-- Dumped by pg_dump version 10.15 (Ubuntu 10.15-0ubuntu0.18.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: vagrant
+--
+
+CREATE TABLE public.projects (
+    id integer NOT NULL,
+    title character varying(30),
+    description text,
+    max_grade integer
+);
+
+
+ALTER TABLE public.projects OWNER TO vagrant;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: vagrant
+--
+
+CREATE SEQUENCE public.projects_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.projects_id_seq OWNER TO vagrant;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vagrant
+--
+
+ALTER SEQUENCE public.projects_id_seq OWNED BY public.projects.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: vagrant
+--
+
+CREATE TABLE public.students (
+    first_name character varying(30),
+    last_name character varying(30),
+    github character varying(30),
+    id integer NOT NULL
+);
+
+
+ALTER TABLE public.students OWNER TO vagrant;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: vagrant
+--
+
+CREATE SEQUENCE public.students_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.students_id_seq OWNER TO vagrant;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vagrant
+--
+
+ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
+
+
+--
+-- Name: projects id; Type: DEFAULT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.projects ALTER COLUMN id SET DEFAULT nextval('public.projects_id_seq'::regclass);
+
+
+--
+-- Name: students id; Type: DEFAULT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: vagrant
+--
+
+COPY public.projects (id, title, description, max_grade) FROM stdin;
+1	Markov	Tweets generated from Markov Chains	50
+2	Blockly	Programmatic Logic Puzzle Game	100
+3	Bernify	Add Bernie to Your Alarm Screen	90
+4	Farm2You	Farmers Market Finder	80
+5	StarGazerLTE	Constellation Finder	90
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: vagrant
+--
+
+COPY public.students (first_name, last_name, github, id) FROM stdin;
+Jane	Hacker	jhacks	1
+Sarah	Developer	sdevelops	3
+\.
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
+--
+
+SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
+--
+
+SELECT pg_catalog.setval('public.students_id_seq', 3, true);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
